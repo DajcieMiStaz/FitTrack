6b97087fdd189d98ea5031b74dc1ad0281bb026a
Bartosz Romanowski <romanowskibartosz90@gmail.com>
2025-10-24 21:32:25 +0200

    Final layout, add BarcodeScanner

--- services/frontend/src/App.tsx
+++ services/frontend/src/App.tsx
@@ -6,6 +6,8 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+// import BarcodeScannerComponent from "./components/BarcodeScannerComponent.tsx";
+
 
 function App() {
   return (
@@ -13,7 +15,7 @@ function App() {
       <ThemeProviderWrapper>
         <Layout>
           <Box>
-            
+              {/*<BarcodeScannerComponent/>*/}
           </Box>
         </Layout>
       </ThemeProviderWrapper>
---
+++ services/frontend/src/components/BarcodeScannerComponent.tsx
@@ -0,0 +1,22 @@
+import {useState} from 'react';
+import {BarcodeScanner} from '@thewirv/react-barcode-scanner';
+
+export default function Test() {
+    const [data, setData] = useState('No result');
+
+    return (
+        <>
+            <BarcodeScanner
+                onSuccess={(text) => setData(text)}
+                onError={(error) => {
+                    if (error) {
+                        console.error(error.message);
+                    }
+                }}
+                onLoad={() => console.log('Video feed has loaded!')}
+                containerStyle={{width: '30%'}}
+            />
+            <p>{data}</p>
+        </>
+    );
+}
